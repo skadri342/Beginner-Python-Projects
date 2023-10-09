20cc7b46812cd3048ad9436f622c697097fb0097
Shams Kadri <quadrishams342@gmail.com>
2023-10-09 19:18:35 -0400

    Finished secret-auction program

--- Secret-Auction/README.md
+++ Secret-Auction/README.md
@@ -6,7 +6,7 @@ The objective is to write a program that will collect the names and bids of diff
 
 ```
 Welcome to the secret auction program. 
-What is your name?: Angela
+What is your name?: Shams
 ```
 ```
 What's your bid?: $123
@@ -19,7 +19,7 @@ yes
 If there are other bidders, the screen should clear, so you can pass your phone to the next person. If there are no more bidders, then the program should display the name of the winner and their winning bid. 
 
 ```
-The winner is Elon with a bid of $55000000000
+The winner is safafg with a bid of $55000000000
 ```
 
 Use your knowledge of Python dictionaries and loops to solve this challenge. 
--- Secret-Auction/main.py
+++ Secret-Auction/main.py
@@ -1,5 +1,36 @@
 import os
 from art import logo
 
+auction_dictionary = {}
+auction_end = False
+
+def auction_winner():
+    highest_bidder = ""
+    highest_bid = 0
+    for key in auction_dictionary:
+        if auction_dictionary[key] >= highest_bid:
+            highest_bid = auction_dictionary[key]
+            highest_bidder = key
+    
+    print(f"The winner is {highest_bidder} with a bid of ${highest_bid}.")
+
+def add_to_auction(name, bid_amount):
+    auction_dictionary[name] = bid_amount
+
 print(logo)
 print("Welcome to the secret auction program.")
+while auction_end == False:
+    name = input("What is your name?: ")
+    bid_amount = int(input("What's your bid?: $"))
+    add_to_auction(name, bid_amount)
+    while True:
+        other_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
+        if other_bidders == "yes":
+            auction_end = False
+            os.system("cls")
+            break
+        elif other_bidders == "no":
+            auction_end = True
+            os.system("cls")
+            break
+auction_winner()
