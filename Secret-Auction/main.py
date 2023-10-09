7beecb76fc09ae23ed0218bc1cdf8b887eb9d92a
Shams Kadri <quadrishams342@gmail.com>
2023-10-09 03:04:01 -0400

    Learning about nesting dictionaries and lists and nesting them in eachother

--- Secret-Auction/main.py
+++ Secret-Auction/main.py
@@ -1,27 +1,60 @@
-programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running as expected.", 
-    "Function": "A piece of code that you can easily call over and over again."
-}
+# programming_dictionary = {
+#     "Bug": "An error in a program that prevents the program from running as expected.", 
+#     "Function": "A piece of code that you can easily call over and over again."
+# }
 
-#Retrieving items from the dictionary.
-print(programming_dictionary["Bug"])
+# #Retrieving items from the dictionary.
+# print(programming_dictionary["Bug"])
 
-#Adding new items to dictionary.
-programming_dictionary["Loop"] = "The action of doing something over and over again."
-print(programming_dictionary)
-
-#Create an empty dictionary.
-empty_dictionary = {}
-
-#Wipe an existing dictionary
-# programming_dictionary = {}
+# #Adding new items to dictionary.
+# programming_dictionary["Loop"] = "The action of doing something over and over again."
 # print(programming_dictionary)
 
-#Edit an item in a dictionary
-programming_dictionary["Bug"] = "A moth in your computer"
-print(programming_dictionary)
+# #Create an empty dictionary.
+# empty_dictionary = {}
 
-#Loop through a dictionary
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+# #Wipe an existing dictionary
+# # programming_dictionary = {}
+# # print(programming_dictionary)
+
+# #Edit an item in a dictionary
+# programming_dictionary["Bug"] = "A moth in your computer"
+# print(programming_dictionary)
+
+# #Loop through a dictionary
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+
+###############################################################
+
+#Nesting
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+#Nesting a List in a Dictionary
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"]
+}
+
+#Nesting Dictionary in a Dictionary
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+
+#Nesting a Dictionary in a List
+travel_log = [
+    {"country": "France", 
+     "cities_visited": ["Paris", "Lille", "Dijon"], 
+     "total_visits": 12,
+    },
+    {"country": "Germany", 
+     "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+     "total_visits": 5,
+    },
+]
