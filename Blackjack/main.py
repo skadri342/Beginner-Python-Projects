238da08c8ea7f44a0841dbf2b0a1660afc83d3cf
Shams Kadri <quadrishams342@gmail.com>
2023-11-09 19:02:35 -0500

    Added score calculating function

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -16,6 +16,8 @@ import random
 import os
 from art import logo
 
+game_end = False
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def deal_card():
@@ -37,3 +39,11 @@ def calculate_score(list):
         list.remove(11)
         list.append(1)
     return sum(list)
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+
+if user_score == 0 or computer_score == 0:
+    game_end = True
+elif user_score > 21:
+    game_end = True
