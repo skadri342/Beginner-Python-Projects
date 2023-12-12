b69819a06f12c217e2450fc89e75da23e4efad14
Shams Kadri <quadrishams342@gmail.com>
2023-12-11 21:57:09 -0500

    added fizzbuzz code to debug

---
+++ Debug-Problems/fizzbuzz.py
@@ -0,0 +1,10 @@
+target = int(input())
+for number in range(1, target + 1):
+  if number % 3 == 0 or number % 5 == 0:
+    print("FizzBuzz")
+  if number % 3 == 0:
+    print("Fizz")
+  if number % 5 == 0:
+    print("Buzz")
+  else:
+    print([number])
